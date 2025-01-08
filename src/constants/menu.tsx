f8eae56f49a7e401cb32bce7b1e9015c75b02988
shashank1623 <shashankbhardwaj2030@gmail.com>
2025-01-08 23:06:20 +0530

    Constant for UI

---
+++ src/constants/menu.tsx
@@ -0,0 +1,35 @@
+
+import { AutomationDuoToneWhite, HomeDuoToneBlue, HomeDuoToneWhite, RocketDuoToneWhite, SettingsDuoToneWhite } from "@/icons"
+import { v4 as uuid } from "uuid"
+type FieldProps = {
+    label : string
+    id : string
+}
+
+type SideBarProps = {
+    icon : React.ReactNode
+} & FieldProps
+
+export const SIDEBAR_MENU : SideBarProps [] = [
+    {
+        id : uuid(),
+        label : 'home',
+        icon : <HomeDuoToneWhite/>
+    },
+    {
+        id : uuid(),
+        label : 'automations',
+        icon : <AutomationDuoToneWhite/>
+    },
+    {
+        id : uuid(),
+        label : 'integrations',
+        icon : <RocketDuoToneWhite/>
+    },
+    {
+        id : uuid(),
+        label : 'Settings',
+        icon : <SettingsDuoToneWhite/>
+    },
+    
+]
