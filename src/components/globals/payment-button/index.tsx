3c462ff73845b7136db4f7f45d4d3ae1cb8486f5
shashank1623 <shashankbhardwaj2030@gmail.com>
2025-01-09 23:52:10 +0530

    Side payment button UI only

---
+++ src/components/globals/payment-button/index.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+type Props = {}
+
+const PaymentButton = (props: Props) => {
+    // WIP : Get their subscription plan and show payment button
+    return (
+        <Button className='bg-gradient-to-br text-white rounded-full from-[#6d60a3] via-[#9434E6] font-bold to-[#CC3BD4]'>
+            Upgrade
+        </Button>
+    )
+}
+
+export default PaymentButton
