6d2477f837a3cb96364cc1e1709ea942fb3ce414
shashank1623 <shashankbhardwaj2030@gmail.com>
2025-01-09 23:54:15 +0530

    navbar info btn svg ui only

---
+++ src/components/globals/infobar/index.tsx
@@ -0,0 +1,19 @@
+import { Button } from '@/components/ui/button'
+import { Bell } from 'lucide-react'
+import React from 'react'
+
+type Props = {}
+
+const Infobar = (props: Props) => {
+  return (
+    <Button className='bg-white rounded-full py-6'>
+        <Bell
+            color='#3352CC'
+            fill='#3352CC'
+        />
+
+    </Button>
+  )
+}
+
+export default Infobar
