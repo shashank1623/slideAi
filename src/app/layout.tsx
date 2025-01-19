f4371fd8933562a365697a601eb232c0b5ee523f
shashank1623 <shashankbhardwaj2030@gmail.com>
2025-01-19 22:37:33 +0530

    React redux provider added for global state management

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import { Plus_Jakarta_Sans } from 'next/font/google'
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-providers";
 import { Toaster } from "@/components/ui/sonner"
+import ReactQueryProvider from "@/providers/react-query-provider";
+import ReduxProvider from "@/providers/redux-provider";
 
 const jakarta = Plus_Jakarta_Sans({ subsets: ['latin'] })
 
@@ -31,8 +33,10 @@ export default function RootLayout({
             defaultTheme="dark"
             disableTransitionOnChange
           >
-            {children}
-            <Toaster/>
+            <ReduxProvider>
+              <ReactQueryProvider>{children}</ReactQueryProvider>
+            </ReduxProvider>
+            <Toaster />
           </ThemeProvider>
         </body>
       </html>
