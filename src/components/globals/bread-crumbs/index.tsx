a9cce77e2c3c4befe0d60bff59fef0f21cae14c0
shashank1623 <shashankbhardwaj2030@gmail.com>
2025-01-21 11:21:19 +0530

    WIP flags completed

--- src/components/globals/bread-crumbs/index.tsx
+++ src/components/globals/bread-crumbs/index.tsx
@@ -1,7 +1,11 @@
-
+"use client"
 import { ChevronRight, PencilIcon } from 'lucide-react'
 import React from 'react'
 import ActivateAutomationButton from '../activate-automation-button'
+import { useQueryAutomation } from '@/hooks/user-queries'
+import { useEditAutomation } from '@/hooks/use-automation'
+import { useMutationDataState } from '@/hooks/use-mutation-data'
+import { Input } from '@/components/ui/input'
 
 type Props = {
     id: string
@@ -9,6 +13,11 @@ type Props = {
 
 const AutomationBreadCrumb = ({ id }: Props) => {
     // WIP : Get the automation data
+    const { data } = useQueryAutomation(id);
+    const { edit, enableEdit, inputRef, isPending } = useEditAutomation(id)
+
+
+    const { latestVariable } = useMutationDataState(['update-automation'])
     // User mutation stuff to update the automations
     return (
         <div className='rounded-full w-full p-5 bg-[#18181B1A] flex justify-between items-center'>
@@ -18,10 +27,33 @@ const AutomationBreadCrumb = ({ id }: Props) => {
                 <ChevronRight className='flex-shrink-0' color='#9B9CA0' />
                 <span className='flex gap-x-3 items-center min-w-0'>
                     {/* WIP : Show the editing data */}
-                    <p className='text-[#9B9CA0] truncate'>This is the Automation Title</p>
-                    <span className='cursor-pointer hover:opacity-75 duration-100 transition flex-shrink-0'>
-                        <PencilIcon size={14} />
-                    </span>
+                    {edit ? (
+                        <Input
+                            ref={inputRef}
+                            placeholder={
+                                isPending ? latestVariable.variables : 'Add a new name'
+                            }
+                            className='bg-transparent h-auto outline-none text-base border-none p-0'
+                        />
+                    ) : (
+                        <p className='text-[#9B9CA0] truncate'>
+                            {latestVariable?.variables
+                                ? latestVariable?.variables.name
+                                : typeof data?.data === 'object' ? data?.data?.name : ''
+                            }
+
+                        </p>
+                    )}
+                    {edit ? (
+                        <></>
+                    ) : (
+                        <span
+                            className="cursor-pointer hover:opacity-75 duration-100 transition flex-shrink-0 mr-4"
+                            onClick={enableEdit}
+                        >
+                            <PencilIcon size={14} />
+                        </span>
+                    )}
                 </span>
             </div>
             <div className='flex items-center gap-x-5 ml-auto'>
@@ -38,7 +70,7 @@ const AutomationBreadCrumb = ({ id }: Props) => {
                 </div>
 
             </div>
-            <ActivateAutomationButton id={id}/>
+            <ActivateAutomationButton id={id} />
         </div>
     )
 }
