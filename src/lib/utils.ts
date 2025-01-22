ddf55c9ef8ee4df46fb1c6036bb9f26c198b3a0c
shashank1623 <shashankbhardwaj2030@gmail.com>
2025-01-22 20:30:14 +0530

    validation and updates in array by checking duplicates docs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,6 +5,12 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+/**
+ * Returns the name of the month corresponding to the given month number.
+ *
+ * @param month - The month number (1 for January, 2 for February, etc.).
+ * @returns The name of the month as a string, or 'Invalid Month' if the month number is not between 1 and 12.
+ */
 export const getMonth = (month: number) => {
 
   const months: string [] =[
@@ -28,6 +34,16 @@ export const getMonth = (month: number) => {
   return months[month - 1];
 }
 
+/**
+ * Validates and updates an array by checking for duplicates.
+ * 
+ * If the element is not found in the array, it adds the element to the array.
+ * If the element is found in the array, it removes the element from the array.
+ * 
+ * @param arr - The array of strings to be validated and updated.
+ * @param el - The string element to be checked for duplication.
+ * @returns The updated array after adding or removing the element.
+ */
 export const duplicateValidation = (arr: string[], el: string) => {
   if (!arr.find((t) => t === el)) {
     arr.push(el)
