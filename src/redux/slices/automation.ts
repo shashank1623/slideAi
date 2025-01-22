600c576533f3ca46eeb40457168b34182ac7d868
shashank1623 <shashankbhardwaj2030@gmail.com>
2025-01-22 20:29:34 +0530

    Slice automation state management docs

--- src/redux/slices/automation.ts
+++ src/redux/slices/automation.ts
@@ -19,6 +19,21 @@ const InitialState : IntialStateTriggerProps = {
     },
 }
 
+/**
+ * Slice for automation state management.
+ * 
+ * @constant
+ * @name AUTOMATION
+ * @type {Slice}
+ * 
+ * @property {string} name - The name of the slice.
+ * @property {InitialState} initialState - The initial state of the slice.
+ * @property {Object} reducers - The reducers for the slice.
+ * @property {Function} reducers.TRIGGER - Reducer to handle the trigger action.
+ * @param {InitialState} state - The current state of the slice.
+ * @param {PayloadAction<IntialStateTriggerProps>} action - The action dispatched to the reducer.
+ * @returns {InitialState} The updated state after applying the trigger action.
+ */
 export const AUTOMATION = createSlice({
     name : 'automation',
     initialState : InitialState,
