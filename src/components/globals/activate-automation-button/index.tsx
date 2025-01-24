581c5f2789c0cb17eb0484605be3197db8a596d6
shashank1623 <shashankbhardwaj2030@gmail.com>
2025-01-24 21:09:43 +0530

    WIP flags completed

--- src/components/globals/activate-automation-button/index.tsx
+++ src/components/globals/activate-automation-button/index.tsx
@@ -1,52 +1,36 @@
 import { Button } from '@/components/ui/button'
-import Loader from '../loader'
+import { Loader2 } from 'lucide-react'
+import React from 'react'
+import { useQueryAutomation } from '@/hooks/user-queries'
+import { useMutationData } from '@/hooks/use-mutation-data'
+import { activateAutomation } from '@/actions/automation'
 import { ActiveAutomation } from '@/icons/active-automation'
 
-
-
 type Props = {
-    id: string
+  id: string
 }
 
-/**
- * ActivateAutomationButton component.
- *
- * This component renders a button that is intended to activate or deactivate an automation process.
- * It includes a loader to indicate the loading state and displays the current state of the automation.
- *
- * @component
- * @param {Object} props - The properties object.
- * @param {string} props.id - The unique identifier for the automation.
- *
- * @example
- * <ActivateAutomationButton id="automation-123" />
- *
- * @remarks
- * - The button currently has a static label "Activate".
- * - The loading state and the actual activation/deactivation logic are work-in-progress (WIP).
- */
 const ActivateAutomationButton = ({ id }: Props) => {
+  const { data } = useQueryAutomation(id)
+  const { mutate, isPending } = useMutationData(
+    ['activate'],
+    (data: { state: boolean }) => activateAutomation(id, data.state),
+    'automation-info'
+  )
 
-    // WIP : setup optimistic ui
-    // fetch automation data
+  return (
+    <Button
+      disabled={isPending}
+      onClick={() => mutate({ state: !data?.data?.active })}
+      className="lg:px-10 bg-gradient-to-br hover:opacity-80 text-white rounded-full from-[#3352CC] font-medium to-[#1C2D70] ml-4"
+    >
+      {isPending ? <Loader2 className="animate-spin" /> : <ActiveAutomation />}
 
-    return (
-        <Button
-            //   disabled={isPending}
-            //   onClick={() => mutate({ state: !data?.data?.active })}
-            className="lg:px-10 bg-gradient-to-br hover:opacity-80 text-white rounded-full from-[#3352CC] font-medium to-[#1C2D70] ml-4"
-        >
-
-            {/* WIP : Set the loading state */}
-            <Loader state={false}>
-                <ActiveAutomation/>
-                <p className="lg:inline hidden">
-                    {/* {data?.data?.active ? 'Disable' : 'Activate'} */}
-                    Acitivate
-                </p>
-            </Loader>
-        </Button>
-    )
+      <p className="lg:inline hidden">
+        {data?.data?.active ? 'Disable' : 'Activate'}
+      </p>
+    </Button>
+  )
 }
 
 export default ActivateAutomationButton
