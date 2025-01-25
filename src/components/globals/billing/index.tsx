3c745fc3e92cb2d4283c7c83044e8fd51bf9d8cf
shashank1623 <shashankbhardwaj2030@gmail.com>
2025-01-25 23:04:35 +0530

    billing wip complelted

--- src/components/globals/billing/index.tsx
+++ src/components/globals/billing/index.tsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import PaymentCard from './payment-card'
+import { useQueryUser } from '@/hooks/user-queries'
 
 type Props = {}
 
 const Billing = (props: Props) => {
     // WIP : Fetch billing information for the customers
+    // WIP : compoleted
+    const { data } = useQueryUser()
     return (
         <div className="flex lg:flex-row flex-col gap-5 w-full lg:w-10/12 xl:w-8/12 container">
-            <PaymentCard label="FREE" current={'FREE'}/>
-            <PaymentCard label="PRO" current={'FREE'}/>
+            <PaymentCard
+                current={data?.data?.subscription?.plan!}
+                label="PRO"
+            />
+            <PaymentCard
+                current={data?.data?.subscription?.plan!}
+                label="FREE"
+            />
         </div>
     )
 }
