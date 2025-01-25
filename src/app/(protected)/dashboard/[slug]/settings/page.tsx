b2f90b07c506fc1f8e6464443856d9bcf0964c77
shashank1623 <shashankbhardwaj2030@gmail.com>
2025-01-25 11:42:16 +0530

    dashboard setting UI layout

---
+++ src/app/(protected)/dashboard/[slug]/settings/page.tsx
@@ -0,0 +1,12 @@
+import Billing from '@/components/globals/billing'
+import React from 'react'
+
+type Props = {}
+
+const Page = (props: Props) => {
+  return (
+    <Billing/>
+  )
+}
+
+export default Page
