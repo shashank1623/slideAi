ab9cc95460c8bb768bb70bf7c4f12c436630c4bc
shashank1623 <shashankbhardwaj2030@gmail.com>
2025-02-05 21:06:17 +0530

    payment loading page

---
+++ src/app/(protected)/payment/loading.tsx
@@ -0,0 +1,12 @@
+import Loader from "@/components/globals/loader"
+
+
+const Loading = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <Loader state>...loading</Loader>
+    </div>
+  )
+}
+
+export default Loading
