be8a67f32a9def43c0820d7b65c61757b96a56dd
shashank1623 <shashankbhardwaj2030@gmail.com>
2025-02-07 23:56:32 +0530

    Query to find user from the db

---
+++ src/actions/user/queries.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import { client } from "@/lib/prisma";
+
+// to create all our queries file
+
+export const findUser = async (clerkId: string) => {
+  return await client.user.findUnique({
+    where: {
+      clerkId,
+    },
+    include: {
+      subscription: true,
+      integrations: {
+        select: {
+          id: true,
+          token: true,
+          expiresAt: true,
+          name: true,
+        },
+      },
+    },
+  });
+};
+
+
