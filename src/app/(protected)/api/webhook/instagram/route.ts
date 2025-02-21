c3b6e3c840796e4fda2153f798dc242f8bbaccc0
shashank1623 <shashankbhardwaj2030@gmail.com>
2025-02-21 20:36:19 +0530

    instagram oauth methond to validate the webhook and change event in dm as well as comments

--- src/app/(protected)/api/webhook/instagram/route.ts
+++ src/app/(protected)/api/webhook/instagram/route.ts
@@ -1,3 +1,4 @@
+import { getKeyWordAutomation, matchKeyword } from "@/actions/webhook/queries";
 import { NextRequest, NextResponse } from "next/server";
 
 // the below function is a method to validate the webhook from instagram
@@ -7,3 +8,38 @@ export async function GET(req : NextRequest){
     return new NextResponse(hub);
 }
 
+
+export async function POST(req : NextRequest){
+    const webhook_payload = await req.json()
+
+    let matcher 
+    try{
+        // messages
+        if(webhook_payload.entry[0].messaging){
+            matcher = await matchKeyword(webhook_payload.entry[0].messaging[0].message.text)
+
+        }
+
+        // comments
+        if(webhook_payload.entry[0].changes){
+            matcher = await matchKeyword(webhook_payload.entry[0].changes[0].value.text)
+        }
+
+
+        if(matcher && matcher.automationId){
+            // we have a keyword matcher
+            // we need to trigger the automation
+
+            if(webhook_payload.entry[0].messaging){
+                // send a message
+                //dm
+
+                const automation = await getKeyWordAutomation(matcher.automationId,true)
+
+            }
+        }
+
+    }catch(error){
+
+    }
+}
