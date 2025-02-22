9498d4e59e909aaff0e9f6c34638e06aa91813da
shashank1623 <shashankbhardwaj2030@gmail.com>
2025-02-22 21:44:23 +0530

    chat history and keyowrd for post

--- src/actions/webhook/queries.ts
+++ src/actions/webhook/queries.ts
@@ -1,75 +1,108 @@
-"use server"
+"use server";
 
-import { client } from "@/lib/prisma"
+import { client } from "@/lib/prisma";
 
-export const matchKeyword = async ( keyword : string) => {
+export const matchKeyword = async (keyword: string) => {
+  return await client.keyword.findFirst({
+    where: {
+      word: {
+        equals: keyword,
+        mode: "insensitive",
+      },
+    },
+  });
+};
 
-    return await client.keyword.findFirst({
-        where : {
-            word :{
-                equals : keyword,
-                mode : 'insensitive'
-            }
-        }
-    })
-}
+export const getKeyWordAutomation = async (
+  automationId: string,
+  dm: boolean
+) => {
+  return await client.automation.findFirst({
+    where: {
+      id: automationId,
+    },
 
-export const getKeyWordAutomation = async (automationId : string , dm : boolean) => {
-
-    return await client.automation.findFirst({
-        where : {
-            id : automationId
+    include: {
+      dms: dm,
+      trigger: {
+        where: {
+          type: dm ? "DM" : "COMMENT",
         },
-
-        include : {
-            dms : dm,
-            trigger : {
-                where : {
-                    type : dm ? 'DM' : 'COMMENT'
-                }
+      },
+      listener: true,
+      User: {
+        select: {
+          subscription: {
+            select: {
+              plan: true,
             },
-            listener : true,
-            User : {
-                select : {
-                    subscription : {
-                        select : {
-                            plan : true,
-                        },
-                    },
-                    integrations : {
-                        select : {
-                            token : true,
-                        }
-                    }
-                },
+          },
+          integrations: {
+            select: {
+              token: true,
             },
+          },
         },
-    })
-}
+      },
+    },
+  });
+};
 
-export const trackResponses = async(
-    automationId : string,
-    type : 'COMMENT' | 'DM'
-) =>{
-    if(type === 'COMMENT'){
-        return await client.listener.update({
-            where : {automationId},
-            data : {
-                commentCount : {
-                    increment : 1,
-                }
-            }
-        })
-    }
+export const trackResponses = async (
+  automationId: string,
+  type: "COMMENT" | "DM"
+) => {
+  if (type === "COMMENT") {
+    return await client.listener.update({
+      where: { automationId },
+      data: {
+        commentCount: {
+          increment: 1,
+        },
+      },
+    });
+  }
 
-    if(type === 'DM'){
-        return await client.listener.update({
-            where : {automationId},
-            data : {
-                dmCount : {
-                    increment : 1,
-                }
-            }
-        })
-    }
-}
+  if (type === "DM") {
+    return await client.listener.update({
+      where: { automationId },
+      data: {
+        dmCount: {
+          increment: 1,
+        },
+      },
+    });
+  }
+};
+
+// create chat history
+export const createChatHistory = (
+  automationId: string,
+  sender: string,
+  reciever: string,
+  message: string
+) => {
+  return client.automation.update({
+    where: {
+      id: automationId,
+    },
+    data: {
+      dms: {
+        create: {
+          reciever,
+          senderId: sender,
+          message,
+        },
+      },
+    },
+  });
+};
+
+export const getKeywordPost = async (postId: string, automationId: string) => {
+  return await client.post.findFirst({
+    where: {
+      AND: [{ postid: postId }, { automationId }],
+    },
+    select: { automationId: true },
+  });
+};
