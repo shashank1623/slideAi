e3701734e84ccc55cac1babe86100d9e6cbb8997
shashank1623 <shashankbhardwaj2030@gmail.com>
2025-02-22 23:12:18 +0530

    instagram integration and queries

---
+++ src/actions/integrations/index.ts
@@ -0,0 +1,11 @@
+"use server"
+
+import { redirect } from "next/navigation"
+
+
+
+export const onOAuthInstagram = async (strategy :  'INSTAGRAM' | 'CRM') => {
+    if(strategy==='INSTAGRAM'){
+        return redirect(process.env.INSTAGRAM_EMBEDDED_OAUTH_URL as string)
+    }
+}
