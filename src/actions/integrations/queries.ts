7141bb9f520e622fa5f24082a814c61dbb7ac72a
shashank1623 <shashankbhardwaj2030@gmail.com>
2025-02-23 23:10:49 +0530

    integration queries of slide

--- src/actions/integrations/queries.ts
+++ src/actions/integrations/queries.ts
@@ -1,16 +1,60 @@
-"use server"
+"use server";
 
-import { client } from "@/lib/prisma"
+import { client } from "@/lib/prisma";
 
-export const updateIntegration = async (token : string , expire : Date ,id : string ) =>{
+export const updateIntegration = async (
+  token: string,
+  expire: Date,
+  id: string
+) => {
+  return await client.integrations.update({
+    where: {
+      id,
+    },
+    data: {
+      token,
+      expiresAt: expire,
+    },
+  });
+};
 
-    return await client.integrations.update({
-        where:{
-            id 
+export const getIntegration = async (clerkId: string) => {
+  return await client.user.findUnique({
+    where: {
+      clerkId,
+    },
+    select: {
+      integrations: {
+        where: {
+          name: "INSTAGRAM",
         },
-        data :{
-            token,
-            expiresAt : expire,
-        }
-    })
-}
+      },
+    },
+  });
+};
+
+export const createIntegration = async (
+  clerkId: string,
+  token: string,
+  expire: Date,
+  igId?: string
+) => {
+  return await client.user.update({
+    where: {
+      clerkId,
+    },
+    data: {
+      integrations: {
+        create: {
+          token,
+          expiresAt: expire,
+          instagramId: igId,
+        },
+      },
+    },
+    select: {
+      firstname: true,
+      lastname: true,
+    },
+  });
+};
